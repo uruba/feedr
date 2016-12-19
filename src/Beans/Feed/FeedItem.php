b2362e146e023f8565e37a0967d587a351803fe3
uruba <uruba@outlook.com>
2016-12-19 16:50:02 +0100

    Read the items in the feed and add them to the respective field on the main feed object

--- src/Beans/Feed/FeedItem.php
+++ src/Beans/Feed/FeedItem.php
@@ -5,4 +5,94 @@ namespace Feedr\Beans\Feed;
 class FeedItem
 {
 
+	private $title;
+
+	private $link;
+
+	private $summary;
+
+	private $content;
+
+	private $author;
+
+	/**
+	 * @return mixed
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * @param mixed $title
+	 */
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getLink()
+	{
+		return $this->link;
+	}
+
+	/**
+	 * @param mixed $link
+	 */
+	public function setLink($link)
+	{
+		$this->link = $link;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getSummary()
+	{
+		return $this->summary;
+	}
+
+	/**
+	 * @param mixed $summary
+	 */
+	public function setSummary($summary)
+	{
+		$this->summary = $summary;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getContent()
+	{
+		return $this->content;
+	}
+
+	/**
+	 * @param mixed $content
+	 */
+	public function setContent($content)
+	{
+		$this->content = $content;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getAuthor()
+	{
+		return $this->author;
+	}
+
+	/**
+	 * @param mixed $author
+	 */
+	public function setAuthor($author)
+	{
+		$this->author = $author;
+	}
+
 }
--- src/Core/Reader.php
+++ src/Core/Reader.php
@@ -3,6 +3,7 @@
 namespace Feedr\Core;
 
 use Feedr\Beans\Feed\Feed;
+use Feedr\Beans\Feed\FeedItem;
 use Feedr\Beans\TempFile;
 use Feedr\Interfaces\InputSource;
 use Feedr\Interfaces\Specs\Spec;
@@ -16,17 +17,29 @@ class Reader
 	/** @var Spec */
 	private $mode;
 
+	/**
+	 * Reader constructor.
+	 * @param Spec $mode
+	 */
 	public function __construct(Spec $mode)
 	{
 		$this->xmlReader = new \XMLReader();
 		$this->switchMode($mode);
 	}
 
+	/**
+	 * @param Spec $mode
+	 */
 	public function switchMode(Spec $mode)
 	{
 		$this->mode = $mode;
 	}
 
+	/**
+	 * @param InputSource $inputSource
+	 * @param string $tempPath
+	 * @param boolean $deleteTempFile
+	 */
 	public function read(InputSource $inputSource, $tempPath, $deleteTempFile = TRUE)
 	{
 		/** @var TempFile $tempFile */
@@ -55,43 +68,81 @@ class Reader
 		// Initialize the info about the feed
 		while ($this->xmlReader->read()) {
 			if ($this->xmlReader->nodeType === \XMLReader::ELEMENT) {
-				$elementName = $this->xmlReader->name;
-				$elementContent = $this->getCurrentElementContent();
-
-				switch ($elementName) {
+				switch ($this->xmlReader->name) {
 					// Feed title
 					case $this->mode->getSpecDocument()->getPathTitle():
-						$feed->setTitle($elementContent);
+						$feed->setTitle($this->getCurrentElementContent());
 						break;
 					// Feed link
 					case $this->mode->getSpecDocument()->getPathLink():
-						$feed->setLink($elementContent);
+						$feed->setLink($this->getCurrentElementContent());
 						break;
 					// Feed created at
 					case $this->mode->getSpecDocument()->getPathCreated():
 						$feed->setCreatedAt($this->convertStringToDateTime(
-							$elementContent
+							$this->getCurrentElementContent()
 						));
 						break;
 					// Feed updated at
 					case $this->mode->getSpecDocument()->getPathUpdated():
 						$feed->setUpdatedAt($this->convertStringToDateTime(
-							$elementContent
+							$this->getCurrentElementContent()
 						));
 						break;
-					case $this->mode->getSpecItem()->getPathRoot():
+					// Items' parent element (no more main feed info past this point)
+					case $this->mode->getSpecItem()->getRoot():
 						break 2;
 				}
 			}
 		}
 
+		// TODO - maybe some logging?
+
+		// Iterate the item nodes
+		do {
+			if ($this->xmlReader->nodeType === \XMLReader::ELEMENT &&
+				$this->xmlReader->name === $this->mode->getSpecItem()->getRoot()) {
+					$xmlElement = new \SimpleXMLElement($this->xmlReader->readOuterXML());
+
+					$feedItem = new FeedItem();
+
+					// Item title
+					$feedItem->setTitle(
+						$this->getSubNodeValue($xmlElement, $this->mode->getSpecItem()->getPathTitle())
+					);
+					// Item link
+					$feedItem->setLink(
+						$this->getSubNodeValue($xmlElement, $this->mode->getSpecItem()->getPathLink())
+					);
+					// Item summary
+					$feedItem->setSummary(
+						$this->getSubNodeValue($xmlElement, $this->mode->getSpecItem()->getPathSummary())
+					);
+					// Item content
+					$feedItem->setContent(
+						$this->getSubNodeValue($xmlElement, $this->mode->getSpecItem()->getPathContent())
+					);
+					// Item author
+					$feedItem->setAuthor(
+						$this->getSubNodeValue($xmlElement, $this->mode->getSpecItem()->getPathAuthor())
+					);
+
+					// Add the item to the item array in the feed object
+					$feed->addItem($feedItem);
+			}
+		} while ($this->xmlReader->next());
+
 		$this->xmlReader->close();
 
+		// Delete the temp file if needed
 		if ($deleteTempFile) {
 			$tempFile->delete();
 		}
 	}
 
+	/**
+	 * @return null|string
+	 */
 	private function getCurrentElementContent()
 	{
 		if ($this->xmlReader->nodeType === \XMLReader::ELEMENT) {
@@ -102,6 +153,20 @@ class Reader
 		return NULL;
 	}
 
+	/**
+	 * @param \SimpleXMLElement $element
+	 * @param string $fieldName
+	 * @return string
+	 */
+	private function getSubNodeValue(\SimpleXMLElement $element, $fieldName)
+	{
+		return (string) $element->{$fieldName};
+	}
+
+	/**
+	 * @param string $dateTimeString
+	 * @return bool|\DateTime
+	 */
 	private function convertStringToDateTime($dateTimeString)
 	{
 		return \DateTime::createFromFormat($this->mode->getDateTimeFormat(), $dateTimeString);
