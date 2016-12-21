b4c21f07d444aa2d45c820aab95233329da841df
uruba <uruba@outlook.com>
2016-12-21 13:05:49 +0100

    Made the HttpInput operational

--- inputs/HttpInput.php
+++ inputs/HttpInput.php
@@ -10,11 +10,16 @@ use GuzzleHttp\Client;
  * Class HttpInput
  * @package Feedr\Inputs
  */
-class HttpInput implements InputSource
+class HttpInput implements InputSource  // TODO - make PSR-7 compatible instead of tying to Guzzle
 {
+
+	const FILENAME_PREFIX = 'http';
 
 	/** @var Client */
 	private $guzzleClient;
+
+	/** @var string */
+	private $httpLink;
 
 	/**
 	 * HttpInput constructor.
@@ -22,13 +27,35 @@ class HttpInput implements InputSource
 	 */
 	public function __construct($httpLink)
 	{
+		$this->httpLink = $httpLink;
+
 		$this->guzzleClient = new Client();
 	}
 
 	/** @return TempFile */
 	public function createTempFile($tempPath)
 	{
-		// TODO: Implement createTempFile() method.
+		// initialize the temp file
+		$urlHost = parse_url($this->httpLink);
+		$fileMeta['base_url'] = isset($urlHost['host']) ? str_replace('.', '_', $urlHost['host']) : '';
+
+		$tempFile = new TempFile($tempPath, self::FILENAME_PREFIX, $fileMeta);
+
+		// populate the temp file
+		$response = $this->makeRequest($this->httpLink);
+
+		file_put_contents($tempFile->getFilePath(), $response);
+
+		return $tempFile;
+	}
+
+	/**
+	 * @param $httpLink
+	 */
+	private function makeRequest($httpLink)
+	{
+		$resource = $this->guzzleClient->request('GET', $httpLink);
+		return $resource->getBody();
 	}
 
 }
