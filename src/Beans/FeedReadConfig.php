806a018f282f3197fcc9dba4cd39e69940e632a1
Václav Uruba <uruba@outlook.com>
2017-04-26 20:28:44 +0200

    Moved the logger initialization down the line from the main project class

--- src/Beans/FeedReadConfig.php
+++ src/Beans/FeedReadConfig.php
@@ -4,6 +4,7 @@ namespace Feedr\Beans;
 
 use Feedr\Interfaces\Spec;
 use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 
 class FeedReadConfig
 {
@@ -19,11 +20,11 @@ class FeedReadConfig
     public function __construct(
         Spec $spec,
         $tempPath,
-        LoggerInterface $logger
+        $logger = null
     ) {
         $this->spec = $spec;
         $this->tempPath = $tempPath;
-        $this->logger = $logger;
+        $this->logger = $this->initLogger($logger);
     }
 
     public function getSpec()
@@ -54,5 +55,14 @@ class FeedReadConfig
     public function setLogger(LoggerInterface $logger)
     {
         $this->logger = $logger;
+    }
+
+    private function initLogger($logger)
+    {
+        if (!($logger instanceof LoggerInterface)) {
+            $logger = new NullLogger();
+        }
+
+        return $logger;
     }
 }
