10e8a3ffd05c2df1606a6efa284c6cd40d33fa9e
Václav Uruba <uruba@outlook.com>
2017-05-05 19:53:04 +0200

    Added a PHPDoc comment

--- src/Exceptions/InvalidComparisionCallbackException.php
+++ src/Exceptions/InvalidComparisionCallbackException.php
@@ -2,7 +2,10 @@
 
 namespace Feedr\Exceptions;
 
+/**
+ * Class InvalidComparisionCallbackException
+ * @package Feedr\Exceptions
+ */
 class InvalidComparisionCallbackException extends \RuntimeException
 {
-
 }
